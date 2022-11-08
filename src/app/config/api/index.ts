793773e6ccaa5991cbef48138cede4ce3214321e
anhnt2989 <anhtn@scuti.asia>
2022-11-08 10:23:23 +0700

    [App] [Chore] - Edited: fixed base url

--- src/app/config/api/index.ts
+++ src/app/config/api/index.ts
@@ -1,5 +1,6 @@
 const apiConfigs = {
-  BASE_URL: process.env.REACT_APP_BE_URL
+  // BASE_URL: process.env.REACT_APP_BE_URL
+  BASE_URL: 'https://www.dnd5eapi.co/api/'
 }
 
 export default apiConfigs
